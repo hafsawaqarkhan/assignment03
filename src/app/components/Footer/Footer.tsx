f632a4752f0afbec1cbd9725f14717b2ab85aec9
hafsawaqarkhan <163749368+hafsawaqarkhan@users.noreply.github.com>
2024-10-31 20:31:21 +0500

    Add files via upload

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='underline italic text-center font font-semibold bg-gray-400
+     bg-origin-padding  bg-cover'>
+      YOU CAN CONTACT ME FOR MORE RECOMMENDATIONS
+
+      
+    </div>
+  )
+}
+
+export default Footer
